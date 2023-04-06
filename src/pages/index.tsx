98fa58985440285544c5984f4ed8d7578e05abfd
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-04-06 00:03:01 -0400

    added tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
+        <h1 className="text-xl font-bold underline">Hello world!</h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
