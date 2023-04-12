badfa50349c06d24ae1947d3fc788ec6aef7c725
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-04-12 16:39:25 -0400

    modified ui

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -26,13 +26,18 @@ export default function Login() {
                 href="/"
                 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 gap-2"
               >
-                <Image src={logo} alt="logo" width={35} className="rounded-lg"/>
+                <Image
+                  src={logo}
+                  alt="logo"
+                  width={35}
+                  className="rounded-lg"
+                />
                 Wazan
               </a>
               <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
-                    Sign in to your account
+                    Enter your email below
                   </h1>
                   <form className="space-y-4 md:space-y-6" action="#">
                     <div>
@@ -48,21 +53,6 @@ export default function Login() {
                         id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                         placeholder="name@company.com"
-                      />
-                    </div>
-                    <div>
-                      <label
-                        htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 "
-                      >
-                        Password
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                       />
                     </div>
                     <div className="flex items-center justify-between">
@@ -81,18 +71,12 @@ export default function Login() {
                           </label>
                         </div>
                       </div>
-                      <a
-                        href="#"
-                        className="text-sm font-medium text-primary-600 hover:underline"
-                      >
-                        Forgot password?
-                      </a>
                     </div>
                     <button
                       type="submit"
-                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      className="w-full border border-5 text-black bg-primary-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center trasnition hover:text-white hover:bg-black focus:ring-4 focus:outline-none focus:ring "
                     >
-                      Sign in
+                      Lets do This 🏋️‍♂️
                     </button>
                   </form>
                 </div>
