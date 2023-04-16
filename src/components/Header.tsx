5df0c59779fa21169c67151c8162c9ed3b86f9be
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-04-16 16:22:04 -0400

    Organized code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,19 +9,18 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Header() {
   return (
     <header className={inter.className}>
-      <div className="mx-auto max-w-screen-xl px-4 py-8 fixed top-0 w-full bg-white bg-opacity-50 backdrop-blur-md border b-1 sm:py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 fixed top-0 w-full bg-white bg-opacity-50 backdrop-blur-md border b-1 sm:pt-6 sm:pb-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <Link href="/dashboard">
             <div className="flex text-center sm:text-left inline-block items-center">
               <div className="rounded-lg truncate">
                 <Image src={logo} alt="Wazan logo" height={30} />
               </div>
-              <h1 className="text-2xl font-bold text-gray-900 sm:text-2xl px-2">
+              <h1 className="text-xl font-bold text-gray-900 sm:text-2xl px-2">
                 Wazan
               </h1>
             </div>
           </Link>
-          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center sticky top-0"></div>
         </div>
       </div>
     </header>
