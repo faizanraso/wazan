b83d76115f91b3c18cfe11b97c9bd4514b9f87b1
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-05-04 11:52:52 -0400

    adding multi select menu

--- src/components/DeleteDataModal.tsx
+++ src/components/DeleteDataModal.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Modal from "react-modal";
+import { ToastContainer, toast } from "react-toastify";
 
 import { Inter } from "next/font/google";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,11 +24,18 @@ export default function DeleteDataModal(props: any) {
   };
 
   async function deleteData(e) {
-    if(date === ""){
-      // return toast 
-    } else if (
-      
-    )
+    if (date === "") {
+      toast.error("Please enter a date 📅", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
   }
 
   return (
@@ -55,36 +64,12 @@ export default function DeleteDataModal(props: any) {
                   }}
                 />
               </div>
-              <div className="relative">
-                <input
-                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                  placeholder="Body Weight"
-                  onChange={(e) => {}}
-                />
-
-                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="h-4 w-4 text-gray-400"
-                    height="1em"
-                    width="1em"
-                    {...props}
-                  >
-                    <path d="M12 3a4 4 0 014 4c0 .73-.19 1.41-.54 2H18c.95 0 1.75.67 1.95 1.56C21.96 18.57 22 18.78 22 19a2 2 0 01-2 2H4a2 2 0 01-2-2c0-.22.04-.43 2.05-8.44C4.25 9.67 5.05 9 6 9h2.54A3.89 3.89 0 018 7a4 4 0 014-4m0 2a2 2 0 00-2 2 2 2 0 002 2 2 2 0 002-2 2 2 0 00-2-2m-6 6v8h5v-2H8v-6H6m7 0v8h3c1.11 0 2-.89 2-2v-.5c0-.57-.25-1.12-.68-1.5.43-.38.68-.93.68-1.5V13c0-1.11-.89-2-2-2h-3m2 2h1v1h-1v-1m0 3h1v1h-1v-1z" />
-                  </svg>
-                </span>
-              </div>
-              <button
-                className="mt-8 inline-block w-3/4 rounded-full bg-sky-600 py-4 text-sm font-bold text-white shadow-xl"
-                onClick={(e) => deleteData(e)}
-              >
-                Add Data
-              </button>
+              <div className="select-menu"></div>
             </form>
           </div>
         </section>
       </Modal>
+      <ToastContainer />
     </div>
   );
 }
