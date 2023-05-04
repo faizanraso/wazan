aa5fae71c610befb0f8e1d5f5dc956f09972f7e2
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2023-05-04 12:07:23 -0400

    UI set up

--- src/components/DeleteDataModal.tsx
+++ src/components/DeleteDataModal.tsx
@@ -49,8 +49,8 @@ export default function DeleteDataModal(props: any) {
       >
         <section className="">
           <div className="p-8 text-center sm:p-10">
-            <p className="text-sm font-semibold uppercase tracking-widest text-sky-500">
-              Add Data
+            <p className="text-sm font-semibold uppercase tracking-widest text-red-500">
+              Delete Data
             </p>
             <form className="mx-auto mb-0 mt-8 max-w-md space-y-4">
               <div className="">
@@ -64,7 +64,78 @@ export default function DeleteDataModal(props: any) {
                   }}
                 />
               </div>
-              <div className="select-menu"></div>
+              <div className="select-menu flex flex-col items-center mb-4">
+                <h3 className="my-4 text-xs font-medium text-red-700">
+                  Select the records you wish to delete 🗑️
+                </h3>
+                <ul className="w-48 text-sm font-medium text-black bg-white border border-gray-200 rounded-lg">
+                  <li className="w-full border-b border-gray-200 rounded-t-lg ">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="vue-checkbox"
+                        type="checkbox"
+                        value=""
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 0"
+                      />
+                      <label
+                        htmlFor="vue-checkbox"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
+                      >
+                        Body Weight
+                      </label>
+                    </div>
+                  </li>
+                  <li className="w-full border-b border-gray-200 rounded-t-lg">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="react-checkbox"
+                        type="checkbox"
+                        value=""
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 "
+                      />
+                      <label
+                        htmlFor="react-checkbox"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
+                      >
+                        Bench PR
+                      </label>
+                    </div>
+                  </li>
+                  <li className="w-full border-b border-gray-200 rounded-t-lg ">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="angular-checkbox"
+                        type="checkbox"
+                        value=""
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 "
+                      />
+                      <label
+                        htmlFor="angular-checkbox"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
+                      >
+                        Squat PR
+                      </label>
+                    </div>
+                  </li>
+                  <li className="w-full border-b border-gray-200 rounded-t-lg">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="laravel-checkbox"
+                        type="checkbox"
+                        value=""
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+                      />
+                      <label
+                        htmlFor="laravel-checkbox"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
+                      >
+                        Deadlift PR
+                      </label>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex items-center"></div>
             </form>
           </div>
         </section>
